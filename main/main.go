8ae8da6d7de945b37a2c330d34795028da3fb31f
no-regret666 <RangelJara195@gmail.com>
2025-05-23 21:09:32 +0800

    day3

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"learn_GeeRPC"
 	"log"
 	"net"
@@ -9,11 +8,26 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct {
+	Num1, Num2 int
+}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := learn_GeeRPC.Register(foo); err != nil {
+		log.Fatalf("Register error: %v", err)
+	}
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
-		log.Fatal("network error:", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 	log.Println("start rpc server on", l.Addr())
 	addr <- l.Addr().String()
@@ -25,21 +39,24 @@ func main() {
 	addr := make(chan string)
 	go startServer(addr)
 	client, _ := learn_GeeRPC.Dial("tcp", <-addr)
-	defer func() { _ = client.Close() }()
+	defer func() {
+		_ = client.Close()
+	}()
 
 	time.Sleep(time.Second)
-	// send request & receive response
+
+	//send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
-				log.Fatal("Call Foo.Sum error:", err)
+				log.Fatalf("Call error: %v", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
